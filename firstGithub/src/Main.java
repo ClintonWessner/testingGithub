8f39d1023dff7f7f51e2d5418a5f89f496d8f533
MUHAMMAD7528 <113187553+MUHAMMAD7528@users.noreply.github.com>
2024-02-20 12:24:38 -0500

    clinton

--- firstGithub/src/Main.java
+++ firstGithub/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.print("Hello Clinton");
+        for(int i = 0; i < 15; i++) {
+            System.out.println("Im 15 years old");
+        }
 
         }
     }
