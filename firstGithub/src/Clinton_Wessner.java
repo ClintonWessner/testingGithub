0e34c68c08a5ee71fe48109ed85eea54f5f5e22f
MUHAMMAD7528 <113187553+MUHAMMAD7528@users.noreply.github.com>
2024-02-20 12:49:03 -0500

    tpe2

--- firstGithub/src/Clinton_Wessner.java
+++ firstGithub/src/Clinton_Wessner.java
@@ -2,9 +2,11 @@ public class Clinton_Wessner {
     public static void main(String[] args) {
 
         System.out.print("Hello Clinton");
-
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Im 15 years old");
         }
 
     }
+}
 
 
